c4c1d8197c33b97610f4666e12cb8a029fca6a2a
celmo <christianelmo@gmail.com>
2023-09-28 12:07:21 +0000

    update command conman init

--- conman/commands/init.py
+++ conman/commands/init.py
@@ -6,18 +6,24 @@ from conman.commands.build import Config
 import os
 
 
-def init() -> int:
-    # Check if .conman-config.yml already exists in current directory
-    if os.path.isfile(".conman-config.yml"):
-        print(
-            "Warning : .conman-config.yml already exists in current directory"
-        )
-        print(
-            "Run : 'conman clean' to remove it first or 'conman init --force' to overwrite it"
-        )
+def init(force=False) -> int:
+    if not force:
+        print("Initializing conman project")
+        # Check if .conman-config.yml already exists in current directory
+        if os.path.isfile(".conman-config.yml"):
+            print(
+                "Warning : .conman-config.yml already exists in current directory"
+            )
+            print(
+                "Run : 'conman clean' to remove it first or 'conman init --force (or -f)' to overwrite it"
+            )
+        else:
+            Config().dump_conman_config_file(filename=CONFIG_FILE)
+        return 0
     else:
+        print("Forced Mode - Reinitializing conman project")
         Config().dump_conman_config_file(filename=CONFIG_FILE)
-    return 0
+        return 0
 
 
 if __name__ == "__main__":
