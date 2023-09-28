7af3f6b9ac22ed3d94e31a7a0f05ae490c192292
celmo <christianelmo@gmail.com>
2023-09-28 12:08:00 +0000

    update command clean

--- conman/commands/clean.py
+++ conman/commands/clean.py
@@ -1,10 +1,34 @@
 from __future__ import annotations
 import os
+from conman.constants import *
+from conman.commands.build import Config
+import shutil
 
 
 def clean() -> int:
-    # Remove .conman-config.yml file if exists in current directory
-    if os.path.isfile(".conman-config.yml"):
-        os.remove(".conman-config.yml")
-        print("Config file removed")
-    return 0
+
+    config = Config().load_conman_config_file(filename=CONFIG_FILE)
+
+    if (
+        config.images.root.generate
+        and config.images.root.conda_environment is not None
+    ):
+        print("Deleteting conda environment file")
+        conda_env_file = config.images.root.conda_environment.environment_file
+        os.remove(f"{conda_env_file}")
+
+    if config.container.devcontainer is not None:
+        print("Deleteting .devcontainer folder")
+        shutil.rmtree("./.devcontainer/")
+    else:
+        if config.images.root.generate:
+            print("Deleteting Dockerfile.root")
+            os.remove("./Dockerfile.root")
+        print("Deleteting Dockerfile.user")
+        os.remove("./Dockerfile.user")
+        print("Deleteting docker-compose.yml")
+        os.remove("./docker-compose.yml")
+
+
+if __name__ == "__main__":
+    clean()
